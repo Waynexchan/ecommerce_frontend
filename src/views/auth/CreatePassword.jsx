4329ce1e169886b16f25772517b69a7d39c64a64
wayne chan <waynestock666@gmail.com>
2024-06-02 16:23:02 +0100

    add Create Password and Forgot Password

--- src/views/auth/CreatePassword.jsx
+++ src/views/auth/CreatePassword.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useSearchParams, useNavigate } from 'react-router-dom'
+import { useSearchParams, useNavigate, Link } from 'react-router-dom'
 import apiInstance from '../../utils/axios'
 
 function CreatePassword() {
@@ -9,53 +9,104 @@ function CreatePassword() {
     const [searchParams] = useSearchParams() 
     const otp = searchParams.get("otp")
     const uidb64 = searchParams.get("uidb64")
+    const [isLoading, setIsLoading] = useState(false)
 
     const handlePasswordSubmit = async (e) => {
+        setIsLoading(true)
         e.preventDefault()
-       
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             alert("Password Does Not Match")
-        }
-        else{
+            setIsLoading(false)
+        } else {
+            setIsLoading(true)
+            
             const formdata = new FormData()
-
             formdata.append('password', password)
             formdata.append('uidb64', uidb64)
             formdata.append('otp', otp)
 
-            try{
+            try {
                 await apiInstance.post(`user/password-change/`, formdata).then((res) => {
                     console.log(res.data);
                     alert("Password Changed Successfully")
                     navigate("/login")
+                    setIsLoading(false)
                 })
-            }catch (error) {
+            } catch (error) {
                 alert("An error occurred while trying to change the password")
+                setIsLoading(false)
             }
         }
     }
 
     return (
-        <div>
-            <h1>Create New Password</h1>
-            <form onSubmit={handlePasswordSubmit}>
-                <input 
-                    type="password" 
-                    placeholder='Enter New Password'
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <br/>
-                <br/>
-                <input 
-                    type="password" 
-                    placeholder='Confirm New Password'
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-                <br />
-                <br />
-                <button type='submit'>Save New Password</button>
-            </form>
-        </div>
+        <section>
+            <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+                <div className="container">
+                    <section className="">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-xl-5 col-md-8">
+                                <div className="card rounded-5">
+                                    <div className="card-body p-4">
+                                        <h3 className="text-center">Create New Password</h3>
+                                        <br />
+
+                                        <div className="tab-content">
+                                            <div
+                                                className="tab-pane fade show active"
+                                                id="pills-login"
+                                                role="tabpanel"
+                                                aria-labelledby="tab-login"
+                                            >
+                                                <form onSubmit={handlePasswordSubmit}>
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="password">
+                                                            Password
+                                                        </label>
+                                                        <input
+                                                            type="password"
+                                                            name="password"
+                                                            value={password}
+                                                            className="form-control"
+                                                            onChange={(e) => setPassword(e.target.value)}
+                                                            required
+                                                        />
+                                                    </div>
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="confirm-password">
+                                                            Confirm Password
+                                                        </label>
+                                                        <input
+                                                            type="password"
+                                                            name="confirm-password"
+                                                            value={confirmPassword}
+                                                            className="form-control"
+                                                            onChange={(e) => setConfirmPassword(e.target.value)}
+                                                            required
+                                                        />
+                                                    </div>
+
+                                                    <button className='btn btn-primary btn-rounded w-100 mb-4' type="submit" disabled={isLoading}>
+                                                        {isLoading ? 'Processing...' : 'Save Password'}
+                                                        <i className="fas fa-check-circle" />
+                                                    </button>
+
+                                                    <div className="text-center">
+                                                        <p>
+                                                            Want to sign in? <Link to="/login">Login</Link>
+                                                        </p>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </section>
     )
 }
 
--- src/views/auth/ForgotPassword.jsx
+++ src/views/auth/ForgotPassword.jsx
@@ -1,30 +1,82 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import apiInstance from '../../utils/axios'
-
+import { useNavigate, Link } from 'react-router-dom'
 
 function ForgotPassword() {
     const [email, setEmail] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
 
-    
-    const handleSubmit = () =>{
-        try{
-            apiInstance.get(`user/password-reset/${email}`).then((res) =>{
-                console.log(res.data);
-            });
-        }catch (error){
-            console.log(error);
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setIsLoading(true)
+        try {
+            const res = await apiInstance.get(`user/password-reset/${email}`)
+            alert("An Email Has been Sent to you.")
+            setIsLoading(false)
+        } catch (error) {
+            alert("Email Does Not Exist")
+            setIsLoading(false)
         }
-    }//call api and proccess response
+    }//call api and process response
 
     return (
-        <div>
-        <h1>Forgot Password</h1>
-        <input 
-        onChange={(e) => setEmail(e.target.value)} type="email" placeholder='Enter Email' name='' id=''/>
-        <br />
-        <br />
-        <button onClick={handleSubmit}>Reset Password</button>
-        </div>
+        <section>
+            <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+                <div className="container">
+                    <section className="">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-xl-5 col-md-8">
+                                <div className="card rounded-5">
+                                    <div className="card-body p-4">
+                                        <h3 className="text-center">Forgot Password</h3>
+                                        <br />
+
+                                        <div className="tab-content">
+                                            <div
+                                                className="tab-pane fade show active"
+                                                id="pills-login"
+                                                role="tabpanel"
+                                                aria-labelledby="tab-login"
+                                            >
+                                                <form onSubmit={handleSubmit}>
+                                                    <div className="form-outline mb-4">
+                                                        <label className="form-label" htmlFor="loginName">
+                                                            Email Address
+                                                        </label>
+                                                        <input
+                                                            type="email"
+                                                            id="loginName"
+                                                            name="email"
+                                                            value={email}
+                                                            className="form-control"
+                                                            onChange={(e) => setEmail(e.target.value)}
+                                                            required
+                                                        />
+                                                    </div>
+
+                                                    <button className='btn btn-primary btn-rounded w-100 mb-4' type="submit" disabled={isLoading}>
+                                                        {isLoading ? 'Processing...' : 'Send Email'}
+                                                        <i className="fas fa-paper-plane" />
+                                                    </button>
+
+                                                    <div className="text-center">
+                                                        <p>
+                                                            Want to sign in? <Link to="/login">Login</Link>
+                                                        </p>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </section>
     )
 }
 
