093d738a677ee0d397ddad26ac712d224fa21f44
wayne chan <waynestock666@gmail.com>
2024-06-07 09:04:03 +0100

    cart cart & checkout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import CreatePassword from './views/auth/CreatePassword'
 import Products from './views/store/Products'
 import ProductDetail from './views/store/ProductDetail'
 import Cart from './views/store/Cart'
+import Checkout from './views/store/Checkout'
 
 import StoreHeader from './views/base/StoreHeader'
 import StoreFooter from './views/base/StoreFooter'
@@ -33,6 +34,7 @@ function App() {
         <Route path='/' element= {<Products />}/>
         <Route path='/detail/:slug/' element= {<ProductDetail />}/>
         <Route path='/cart/' element= {<Cart />}/>
+        <Route path='/checkout/:order_oid' element= {<Checkout />}/>
 
       </Routes>
       <StoreFooter/>
--- src/views/store/Cart.jsx
+++ src/views/store/Cart.jsx
@@ -4,29 +4,55 @@ import { useState, useEffect } from 'react';
 import apiInstance from '../../utils/axios';
 import UserData from '../plugin/UserData';
 import CartID from '../plugin/CartID';
+import GetCurrentAddress from '../plugin/UserCountry';
+import swal from 'sweetalert2'
+import { useNavigate  } from 'react-router-dom';
+
+const Toast = swal.mixin({
+    toast:true,
+    position:"top",
+    showConfirmButton:false,
+    timer:1500,
+    timerProgressBar:true
+  })
 
 function Cart() {
     const [cart, setCart] = useState([]);
     const [cartTotal, setCartTotal] = useState([]);
+    const [productQuantities, setProductQuantities] =useState('')
 
 
     const userData = UserData();
     const cart_id = CartID();
+    const currentAddress = GetCurrentAddress()
 
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    const [mobile, setMobile] = useState("")
+
+    const [address, setAddress] = useState("")
+    const [city, setCity] = useState("")
+    const [state, setState] = useState("")
+    const [country, setCountry] = useState("")
+    const navigate = useNavigate()
+
+
+    
+    const fetchCartData = (cartId, userId) => {
+        const url = userId ? `cart-list/${cartId}/${userId}` : `cart-list/${cartId}/`;
+        apiInstance.get(url).then((res) => {
+            setCart(res.data);
+        });
+    };
+
+    const fetchCartTotal = (cartId, userId) => {
+        const url = userId ? `cart-detail/${cartId}/${userId}` : `cart-detail/${cartId}/`;
+        apiInstance.get(url).then((res) => {
+            setCartTotal(res.data);
+        });
+    };
+    
     useEffect(() => {
-        const fetchCartData = (cartId, userId) => {
-            const url = userId ? `cart-list/${cartId}/${userId}` : `cart-list/${cartId}/`;
-            apiInstance.get(url).then((res) => {
-                setCart(res.data);
-            });
-        };
-
-        const fetchCartTotal = (cartId, userId) => {
-            const url = userId ? `cart-detail/${cartId}/${userId}` : `cart-detail/${cartId}/`;
-            apiInstance.get(url).then((res) => {
-                setCartTotal(res.data);
-            });
-        };
 
         if (cart_id) {
             if (userData?.user_id) {
@@ -39,8 +65,143 @@ function Cart() {
         }
     }, [cart_id, userData?.user_id]);
 
-    console.log(cartTotal)
+    useEffect(() =>{
+        const initialQuantities = {}
+        cart.forEach((c) => {
+            initialQuantities[c.product?.id] = c.qty
+        })
+        setProductQuantities(initialQuantities)
+    }, [cart])
 
+    const handleQuantityChange = (event, product_id) =>{
+        const quantity = event.target.value
+        
+
+        setProductQuantities((prevQuantities) =>({
+            ...prevQuantities,
+            [product_id] : quantity
+        }))
+    }
+
+    const updateCart = async(product_id, price, shipping_amount, color, size) =>{
+        const qtyValue = productQuantities[product_id]
+
+        const formdata = new FormData()
+
+        formdata.append("product_id", product_id)
+        formdata.append("user_id", userData?.user_id)
+        formdata.append("qty", qtyValue)
+        formdata.append("price", price)
+        formdata.append("shipping_amount",shipping_amount)
+        formdata.append("country", currentAddress.country)
+        formdata.append("size", color)
+        formdata.append("color", size)
+        formdata.append("cart_id", cart_id)
+
+        const response = await apiInstance.post('cart-view/', formdata)
+        
+
+        fetchCartData(cart_id, userData.user_id);
+        fetchCartTotal(cart_id, userData.user_id);
+        
+        Toast.fire({
+            icon: "success",
+            title: response.data.message
+          })
+          
+    }
+
+    const handleDeleteCartItem = async (itemId) => {
+        const url = userData?.user_id
+            ?`cart-delete/${cart_id}/${itemId}/${userData?.user_id}/`
+            :`cart-delete/${cart_id}/${itemId}/`
+
+            try {
+                await apiInstance.delete(url)
+
+            fetchCartData(cart_id, userData.user_id);
+            fetchCartTotal(cart_id, userData.user_id);
+
+            Toast.fire({
+                icon: "success",
+                title: "Item Removed From Cart"
+              })
+            } catch (error) {
+               console.log(error) 
+            }
+
+    }
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+
+        switch (name){
+            case 'fullName':
+                setFullName(value)
+                break
+
+            case 'email':
+                setEmail(value)
+                break
+
+            case 'mobile':
+                setMobile(value)
+                break
+
+            case 'address':
+                setAddress(value)
+                break
+
+            case 'city':
+                setCity(value)
+                break
+
+            case 'state':
+                setState(value)
+                break
+
+            case 'country':
+                setCountry(value)
+                break
+            
+            default:
+                break
+        }
+    }
+
+    const createOrder = async () =>{
+        
+        if (!fullName || !email || !mobile || !address || !city || !state || !country){
+            swal.fire({
+                icon: 'warning',
+                title: "Missing Fields",
+                text: "All fields are required before checkout!"
+            })
+        } else{
+            try {
+                const formdata = new FormData()
+                formdata.append("full_name", fullName)
+                formdata.append("email", email)
+                formdata.append("mobile", mobile)
+                formdata.append("address", address)
+                formdata.append("city", city)
+                formdata.append("state", state)
+                formdata.append("country", country)
+                formdata.append("cart_id", cart_id)
+                formdata.append("user_id", userData ? userData ?.user_id: 0 )
+                
+                const response = await apiInstance.post('create-order/', formdata)
+                console.log(response.data.order_id)
+                navigate(`/checkout/${response.data.order_oid}`)
+            } catch (error) {
+                console.log(error)
+            }
+            
+        }
+    }
+
+        
+    
     return (
         <div>
             <main className="mt-5">
@@ -97,7 +258,9 @@ function Cart() {
                                                             <span>{c.product?.vendor?.name}</span>
                                                         </p>
                                                         <p className="mt-3">
-                                                            <button className="btn btn-danger ">
+                                                            <button className="btn btn-danger " onClick={() =>{
+                                                                handleDeleteCartItem(c.id)
+                                                            }}>
                                                                 <small><i className="fas fa-trash me-2" />Remove</small>
                                                             </button>
                                                         </p>
@@ -109,12 +272,13 @@ function Cart() {
                                                                     type="number"
                                                                     id='typeNumber'
                                                                     className="form-control"
-                                                                    value={c.qty}
+                                                                    value={productQuantities[c.product?.id] || c.qty}
                                                                     min={1}
+                                                                    onChange={(e) =>handleQuantityChange(e, c.product.id)}
                                                                     
                                                                 />
                                                             </div>
-                                                            <button className='ms-2 btn btn-primary'><i className='fas fa-rotate-right'></i></button>
+                                                            <button  onClick={() => updateCart(c.product.id, c.product.price, c.product.shipping_amount, c.color, c.size)} className='ms-2 btn btn-primary'><i className='fas fa-rotate-right'></i></button>
                                                         </div>
                                                         <h5 className="mb-2 mt-3 text-center"><span className="align-middle">${c.total}</span></h5>
                                                     </div>
@@ -136,7 +300,14 @@ function Cart() {
                                                 <div className="col">
                                                     <div className="form-outline">
                                                         <label className="form-label" htmlFor="full_name"> <i className='fas fa-user'></i> Full Name</label>
-                                                        <input type="text" id="" name='fullName' className="form-control" />
+                                                        <input 
+                                                        type="text" 
+                                                        id="" 
+                                                        name='fullName' 
+                                                        className="form-control"  
+                                                        onChange={handleChange}
+                                                        value={fullName}
+                                                        />
                                                     </div>
                                                 </div>
                                             </div>
@@ -144,13 +315,27 @@ function Cart() {
                                                 <div className="col">
                                                     <div className="form-outline">
                                                         <label className="form-label" htmlFor="form6Example1"><i className='fas fa-envelope'></i> Email</label>
-                                                        <input type="text" id="form6Example1" className="form-control" name='email' />
+                                                        <input 
+                                                        type="text" 
+                                                        id="form6Example1" 
+                                                        className="form-control" 
+                                                        name='email' 
+                                                        onChange={handleChange} 
+                                                        value={email}
+                                                        />
                                                     </div>
                                                 </div>
                                                 <div className="col">
                                                     <div className="form-outline">
                                                         <label className="form-label" htmlFor="form6Example1"><i className='fas fa-phone'></i> Mobile</label>
-                                                        <input type="text" id="form6Example1" className="form-control" name='mobile' />
+                                                        <input 
+                                                            type="text" 
+                                                            id="form6Example1" 
+                                                            className="form-control" 
+                                                            name='mobile' 
+                                                            onChange={handleChange} 
+                                                            value={mobile}
+                                                        />
                                                     </div>
                                                 </div>
                                             </div>
@@ -159,25 +344,53 @@ function Cart() {
                                                 <div className="col-lg-6 mt-3">
                                                     <div className="form-outline">
                                                         <label className="form-label" htmlFor="form6Example1"> Address</label>
-                                                        <input type="text" id="form6Example1" className="form-control" name='address' />
+                                                        <input 
+                                                            type="text" 
+                                                            id="form6Example1" 
+                                                            className="form-control" 
+                                                            name='address' 
+                                                            onChange={handleChange}
+                                                            value={address}
+                                                        />
                                                     </div>
                                                 </div>
                                                 <div className="col-lg-6 mt-3">
                                                     <div className="form-outline">
                                                         <label className="form-label" htmlFor="form6Example1"> City</label>
-                                                        <input type="text" id="form6Example1" className="form-control" name='city' />
+                                                        <input 
+                                                            type="text" 
+                                                            id="form6Example1" 
+                                                            className="form-control" 
+                                                            name='city' 
+                                                            onChange={handleChange}
+                                                            value={city}
+                                                        />
                                                     </div>
                                                 </div>
                                                 <div className="col-lg-6 mt-3">
                                                     <div className="form-outline">
                                                         <label className="form-label" htmlFor="form6Example1"> State</label>
-                                                        <input type="text" id="form6Example1" className="form-control" name='state' />
+                                                        <input 
+                                                            type="text"
+                                                            id="form6Example1" 
+                                                            className="form-control" 
+                                                            name='state' 
+                                                            onChange={handleChange}
+                                                            value={state}
+                                                        />
                                                     </div>
                                                 </div>
                                                 <div className="col-lg-6 mt-3">
                                                     <div className="form-outline">
                                                         <label className="form-label" htmlFor="form6Example1"> Country</label>
-                                                        <input type="text" id="form6Example1" className="form-control" name='country' />
+                                                        <input 
+                                                            type="text" 
+                                                            id="form6Example1" 
+                                                            className="form-control" 
+                                                            name='country' 
+                                                            onChange={handleChange} 
+                                                            value={country}
+                                                        />
                                                     </div>
                                                 </div>
                                             </div>
@@ -210,7 +423,7 @@ function Cart() {
                                                 <span>Total </span>
                                                 <span>${cartTotal.total?.toFixed(2)}</span>
                                             </div>
-                                            <button className="btn btn-primary btn-rounded w-100">
+                                            <button className="btn btn-primary btn-rounded w-100" onClick={createOrder}>
                                                 Go to checkout <i className='fas fa-arrow-right'></i>
                                             </button>
                                         </section>
